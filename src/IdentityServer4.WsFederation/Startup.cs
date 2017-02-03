5d23430c5c24d0f71f713843434ec8d6a04c2f1a
Dominick Baier <dbaier@leastprivilege.com>
2017-02-03 17:26:54 +0100

    add Google login

--- src/IdentityServer4.WsFederation/Startup.cs
+++ src/IdentityServer4.WsFederation/Startup.cs
@@ -28,6 +28,23 @@ namespace IdentityServer4.WsFederation
 
             app.UseIdentityServer();
 
+            // cookie middleware for temporarily storing the outcome of the external authentication
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+                AutomaticAuthenticate = false,
+                AutomaticChallenge = false
+            });
+
+            // middleware for google authentication
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                AuthenticationScheme = "Google",
+                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+                ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com",
+                ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh"
+            });
+
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
