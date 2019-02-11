457aefbf3d719f0635af45bbcab2e731893833e1
Alexej Kowalew <616b2f@gmail.com>
2019-02-11 17:44:36 +0100

    add back mvc owin client from upstream project

---
+++ samples/clients/MvcOwinWsFederation/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOwinWsFederation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Claims()
+        {
+            ViewBag.Message = "Claims";
+
+            return View();
+        }
+
+        public ActionResult SignOut()
+        {
+            Request.GetOwinContext().Authentication.SignOut();
+            return View();
+        }
+
+        public ActionResult SignOutCleanup()
+        {
+            // this should only signout of cookies since it will be running in an iframe
+            Request.GetOwinContext().Authentication.SignOut("Cookies");
+            return new EmptyResult();
+        }
+    }
+}
---
+++ samples/clients/MvcOwinWsFederation/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+
+[assembly: OwinStartup(typeof(MvcOwinWsFederation.Startup))]
+
+namespace MvcOwinWsFederation
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseWsFederationAuthentication(new WsFederationAuthenticationOptions
+            {
+                MetadataAddress = "http://localhost:5000/wsfederation",
+                Wtrealm = "urn:owinrp",
+
+                SignInAsAuthenticationType = "Cookies"
+            });
+        }
+    }
+}
