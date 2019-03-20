9b28c90f46d88dd061d62f090fcc100dd9c18f3d
Alexej Kowalew <alexej.kowalew@gmail.com>
2019-03-20 23:52:38 +0100

    added sharepoint client for testing

--- samples/server/IdentityServer4.WsFederation/Config.cs
+++ samples/server/IdentityServer4.WsFederation/Config.cs
@@ -1,6 +1,9 @@
 using IdentityModel;
 using IdentityServer4.Models;
+using IdentityServer4.WsFederation.Stores;
+using Microsoft.IdentityModel.Tokens;
 using System.Collections.Generic;
+using System.Security.Claims;
 using static IdentityServer4.IdentityServerConstants;
 
 namespace IdentityServer4.WsFederation
@@ -50,6 +53,50 @@ namespace IdentityServer4.WsFederation
                     IdentityTokenLifetime = 36000,
 
                     AllowedScopes = { "openid", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "urn:sharepoint",
+                    ProtocolType = ProtocolTypes.WsFederation,
+
+                    RedirectUris = { "http://sp16lts:9998/_trust/default.aspx" },
+                    IdentityTokenLifetime = 36000,
+
+                    AllowedScopes = { "openid", "profile" }
+                },
+            };
+        }
+
+        public static IEnumerable<RelyingParty> GetRelyingParties()
+        {
+            return new[]
+            {
+                new RelyingParty
+                {
+                    Realm = "urn:sharepoint",
+
+                    TokenType = WsFederationConstants.TokenTypes.Saml2TokenProfile11,
+
+                    // Transform claim types
+                    ClaimMapping = new Dictionary<string, string>
+                    {
+                        { JwtClaimTypes.Name, ClaimTypes.Name },
+                        { JwtClaimTypes.Subject, ClaimTypes.NameIdentifier },
+                        { JwtClaimTypes.Email, ClaimTypes.Email },
+                        //{ JwtClaimTypes.GivenName, ClaimTypes.GivenName },
+                        //{ JwtClaimTypes.FamilyName, ClaimTypes.Surname },
+                        //{ JwtClaimTypes.BirthDate, ClaimTypes.DateOfBirth },
+                        //{ JwtClaimTypes.WebSite, ClaimTypes.Webpage },
+                        //{ JwtClaimTypes.Gender, ClaimTypes.Gender },
+                    },
+
+                    //Encryption
+                    //EncryptionCertificate = new X509Certificate2(Base64Url.Decode("MIIDBTCCAfGgAwIBAgIQNQb+T2ncIrNA6cKvUA1GWTAJBgUrDgMCHQUAMBIxEDAOBgNVBAMTB0RldlJvb3QwHhcNMTAwMTIwMjIwMDAwWhcNMjAwMTIwMjIwMDAwWjAVMRMwEQYDVQQDEwppZHNydjN0ZXN0MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqnTksBdxOiOlsmRNd+mMS2M3o1IDpK4uAr0T4/YqO3zYHAGAWTwsq4ms+NWynqY5HaB4EThNxuq2GWC5JKpO1YirOrwS97B5x9LJyHXPsdJcSikEI9BxOkl6WLQ0UzPxHdYTLpR4/O+0ILAlXw8NU4+jB4AP8Sn9YGYJ5w0fLw5YmWioXeWvocz1wHrZdJPxS8XnqHXwMUozVzQj+x6daOv5FmrHU1r9/bbp0a1GLv4BbTtSh4kMyz1hXylho0EvPg5p9YIKStbNAW9eNWvv5R8HN7PPei21AsUqxekK0oW9jnEdHewckToX7x5zULWKwwZIksll0XnVczVgy7fCFwIDAQABo1wwWjATBgNVHSUEDDAKBggrBgEFBQcDATBDBgNVHQEEPDA6gBDSFgDaV+Q2d2191r6A38tBoRQwEjEQMA4GA1UEAxMHRGV2Um9vdIIQLFk7exPNg41NRNaeNu0I9jAJBgUrDgMCHQUAA4IBAQBUnMSZxY5xosMEW6Mz4WEAjNoNv2QvqNmk23RMZGMgr516ROeWS5D3RlTNyU8FkstNCC4maDM3E0Bi4bbzW3AwrpbluqtcyMN3Pivqdxx+zKWKiORJqqLIvN8CT1fVPxxXb/e9GOdaR8eXSmB0PgNUhM4IjgNkwBbvWC9F/lzvwjlQgciR7d4GfXPYsE1vf8tmdQaY8/PtdAkExmbrb9MihdggSoGXlELrPA91Yce+fiRcKY3rQlNWVd4DOoJ/cPXsXwry8pWjNCo5JD8Q+RQ5yZEy7YPoifwemLhTdsBz3hlZr28oCGJ3kbnpW0xGvQb3VHSTVVbeei0CfXoW6iz1")),
+
+                    // Defaults
+                    DigestAlgorithm = SecurityAlgorithms.Sha256Digest,
+                    SignatureAlgorithm = SecurityAlgorithms.RsaSha256Signature,
+                    SamlNameIdentifierFormat = WsFederationConstants.SamlNameIdentifierFormats.UnspecifiedString
                 }
             };
         }
